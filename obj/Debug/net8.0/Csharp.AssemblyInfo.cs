a8692d688f6e7c01fec355c4de2547d68d8da56c
EudesNilton <eudes258@gmail.com>
2024-04-22 20:42:37 -0300

    aula08

--- obj/Debug/net8.0/Csharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+285720fc1095122f0a249a27f817f83fd0ed5db1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5a7a5ff45fa4abbb9403f6c706489736662ac7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
